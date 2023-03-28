cf1151726142326d8d9ceb66f8cbe582ac0adf6c
Gianne the Weirdo <118273693+giang-doan-ntijohanneberg@users.noreply.github.com>
2023-03-28 14:30:07 +0200

    skapa menu icon

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menu_icon = document.getElementById('menu_icon')
+const nav = document.getElementById('nav_links')
+
+function toggleMenu() {
+    nav.classList.toggle('nav_active')
+    menu_icon.classList.toggle('tryck_menu')
+}
+
+menu_icon.addEventListener('click', toggleMenu)
