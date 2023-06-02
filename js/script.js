3dba5e1605b6d8705786557667206376d7784838
Gianne the Weirdo <118273693+giang-doan-ntijohanneberg@users.noreply.github.com>
2023-06-02 14:41:54 +0200

    .

--- js/script.js
+++ js/script.js
@@ -10,19 +10,3 @@ menu_icon.addEventListener('click', toggleMenu)
 
 
 
-/* const sliderImages = document.querySelector('.product_container');
-const prevBtn = document.querySelector('.pre_btn');
-const nextBtn = document.querySelector('.nxt_btn');
-let currentPosition = 0;
-const imageWidth = 600;
-
-nextBtn.addEventListener('click', () => {
-  currentPosition -= imageWidth;
-  sliderImages.style.transform = `translateX(${currentPosition}px)`;
-});
-
-prevBtn.addEventListener('click', () => {
-  currentPosition += imageWidth;
-  sliderImages.style.transform = `translateX(${currentPosition}px)`;
-}); */
-
